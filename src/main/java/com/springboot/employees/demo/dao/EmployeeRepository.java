b6bd6059828cc7f46548cb8539f5acd10fe7a38f
nchen0 <nicky.chen0@gmail.com>
2019-08-07 19:29:22 -0400

    spring data rest

--- src/main/java/com/springboot/employees/demo/dao/EmployeeRepository.java
+++ src/main/java/com/springboot/employees/demo/dao/EmployeeRepository.java
@@ -2,7 +2,9 @@ package com.springboot.employees.demo.dao;
 
 import com.springboot.employees.demo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 }
